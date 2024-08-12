035f16d8dafbff94cd32b12cf8c6a8773bc0c882
Aditya Dey <deyaditya34@gmail.com>
2024-08-13 02:53:02 +0530

    fd terminal command recursion in process

--- fd_terminal_command.js
+++ fd_terminal_command.js
@@ -2,7 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 function findFiles(directory) {
-    var readDir = fs.readdirSync(directory);
-    console.log(readDir);
+    if (!fs.existsSync(directory)) {
+        return "directory not found";
+    }
+    var isDirectory = fs.statSync(directory).isDirectory();
+    if (!isDirectory) {
+        console.log(pathbeautify(directory));
+    }
+    if (isDirectory) {
+        var directoriesList = fs.readdirSync(directory);
+        var selectedDirectories = directoriesList.filter(function (dir) { return (!/^\./gi.test(dir) || !/node_modules/.test(dir)); });
+        for (var i = 0; i < selectedDirectories.length; i++) {
+            var newDirectory = directory + "/".concat(selectedDirectories[i]);
+            findFiles(newDirectory);
+        }
+    }
+    return "file listing completed";
 }
-findFiles("/home/aditya/Documents/NoSQLBooster");
+function pathbeautify(path) {
+    var result = "";
+    for (var i = 1; i < path.length; i++) {
+        if (path[i] === "/") {
+            result += "--";
+        }
+        else {
+            result += path[i];
+        }
+    }
+    return result;
+}
+// console.log(pathbeautify("/Users/adityadey/Documents/program"));
+console.log(findFiles("/Users/adityadey/Documents/program/"));
--- fd_terminal_command.ts
+++ fd_terminal_command.ts
@@ -1,9 +1,43 @@
-import * as fs from "fs"
+import * as fs from "fs";
 
 function findFiles(directory: string) {
-  const readDir = fs.readdirSync(directory);
-  
-  console.log(readDir)
+  if (!fs.existsSync(directory)) {
+    return "directory not found";
+  }
+
+  const isDirectory: boolean = fs.statSync(directory).isDirectory();
+
+  if (!isDirectory) {
+    console.log(pathbeautify(directory));
+  }
+
+  if (isDirectory) {
+    const directoriesList: string[] = fs.readdirSync(directory);
+    const selectedDirectories: string[] = directoriesList.filter(
+      (dir) => (!/^\./gi.test(dir) || !/node_modules/.test(dir))
+    );
+
+    for (let i = 0; i < selectedDirectories.length; i++) {
+      const newDirectory: string = directory + `/${selectedDirectories[i]}`;
+      findFiles(newDirectory);
+    }
+  }
+
+  return "file listing completed";
 }
 
-findFiles("/home/aditya/Documents/NoSQLBooster")
+function pathbeautify(path: string): string {
+  let result = "";
+
+  for (let i = 1; i < path.length; i++) {
+    if (path[i] === "/") {
+      result += "--";
+    } else {
+      result += path[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(findFiles("/Users/adityadey/Documents/program/"));
